a1e6ead53bbeee299e0c5454bd6e114fdfa2c77f
Y-jiji <y.jijiji.data.science@gmail.com>
2024-11-01 14:31:54 -0400

    Update: parallel/batch-mode encoder/decoder.

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from encoder import *
 from decoder import *
 
 if __name__ == '__main__':
-    encoder = LubyEncoder(np.array([0.5, 0.25, 0.25]), 1024)
+    encoder = LubyEncoder(np.array([0.5, 0.25, 0.25]), 1024, 10000)
     encoder.put_bat(np.ones((1000, 1024), dtype=np.uint8))
     decoder = LubyDecoder(3, 1024)
     for i in range(5):
-        print(i)
         decoder.put_bat(encoder.get_bat(10000))
-    print(decoder.get_all())
+    print((decoder.get_all() == np.ones((1000, 1024), dtype=np.uint8)).all())
