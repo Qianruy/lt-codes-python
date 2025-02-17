81a8ba80d11df9d9461391f4b2bae23eabac44ee
Qianruy <qianruy99@gmail.com>
2025-02-16 21:59:28 -0500

    update decoder

--- decoder/iterative_decoder.py
+++ decoder/iterative_decoder.py
@@ -1,6 +1,37 @@
 from .base_decoder import *
-from numba import njit
+from numba import njit, prange
 from logging import *
+
+@njit(parallel = True, nogil=True)
+def update_buffer(buff_index, buff_degree, buff_data, indices_set, data):
+    """ 
+    Optimized update function using Numba to process buffer modifications. 
+    """
+    num_codewords = buff_data.shape[0]
+    num_source = data.shape[0]
+
+    # Precompute indices as a set for fast lookups
+    indices = set(indices_set)
+
+    for i in prange(num_codewords):  # prange enables parallelism
+        links = np.zeros(buff_index[i].size, dtype=np.bool_)
+        for j in range(buff_index[i].size):
+            if buff_index[i, j] in indices:
+                links[j] = True
+
+        if np.any(links):  
+            buff_index[i] *= ~links  # remove matched indices
+            buff_degree[i] -= np.count_nonzero(links)  # reduce degree
+
+            # Perform XOR reduction (manual for speed)
+            removal = np.zeros(num_source, dtype=np.bool_)
+            for j in prange(1, num_source+1):
+                if j in buff_index[i][links]:
+                    removal[j] = True
+            xor_value = np.uint8(0)
+            for value in data[removal][0]:
+                xor_value ^= value
+            buff_data[i] = xor_value
 
 class IterativeDecoder(Decoder):
     """
@@ -41,15 +72,17 @@ class IterativeDecoder(Decoder):
     
     def get_all(self) -> Optional[int]:
         round = 0
+        buff = self.buff
+
         while True:
             round += 1
-            ripple = self.buff.degree == 1
+            ripple = (buff.degree == 1)
             # loop runs until no codewords of degree 1 are left 
-            if np.all(~ripple): break
+            if not np.any(ripple): break
 
             # put data from degree=1 codewords to inputs
-            index = np.bitwise_or.reduce(self.buff.index[ripple, :], axis=-1)
-            self.data[index, :] = self.buff.data[ripple, :]
+            index = np.bitwise_or.reduce(buff.index[ripple, :], axis=-1)
+            self.data[index, :] = buff.data[ripple, :]
 
             # remove existing index from codewords
             index = np.unique(index)
@@ -59,53 +92,17 @@ class IterativeDecoder(Decoder):
             print("ripple size: {}".format(np.count_nonzero(index)))
             index = index * ~self.collected[index]
             self.collected[index] = True
-            # indices = self.buff.index[ripple, :].flatten()
-            # unique_indices = np.unique(indices)
-            # unique_indices = unique_indices[~self.collected[unique_indices]]
-            # self.data[unique_indices, :] = self.buff.data[ripple, :]
-            # self.collected[unique_indices] = True
-            # # Compute updates for all codewords in one step
-            # links = self.buff.index[:, None, :] == index[:, None]
-            # valid_links = links.sum() > 0
-            # Update buffer using optimized numba function
-            
-            def update_buffer(buff_index, buff_degree, buff_data, indices, data):
-                """ Optimized update function using Numba to process buffer modifications. """
-                num_codewords = buff_data.shape[0]
-                num_source = data.shape[0]
-
-                for i in range(num_codewords):
-                    links = np.isin(buff_index[i], indices)  # mask for linked indices
-                    
-                    if np.any(links):  
-                        buff_index[i] *= ~links  # remove matched indices
-                        buff_degree[i] -= np.count_nonzero(links)  # reduce degree
-
-                        # Perform XOR reduction to update data
-                        removal = np.isin(range(1, num_source+1), buff_index[i][links])
-                        buff_data[i] ^= np.bitwise_xor.reduce(data[removal], axis=0)
 
             update_buffer(self.buff.index, self.buff.degree, self.buff.data, index, self.data)
-            # @delayed
-            # def fill(i):
-            #     # check if unique indices resolved in the current iteration
-            #     # could update the codewords in the buffer
-            #     links = index == self.buff.index[i].reshape(-1, 1)
-            #     # exist z: links[y][z] = true => clip buff.index[y] = 0
-            #     # exist y: links[y][z] = true <=> index[z] is inside codeword i
-            #     # links[y][z] = true <=> self.buff.index[i][y] == unique_index[z]
-            #     if links.sum() >= 1: 
-            #         self.buff.index[i] *= links.sum(-1, dtype=np.int64) < 1
-            #         self.buff.degree[i] -= links.sum()
-            #         removal = np.bitwise_xor.reduce(self.data[index * links.sum(-2)], axis=0)
-            #         self.buff.data[i] = np.bitwise_xor(self.buff.data[i], removal)
-            # Parallel(n_jobs=4, require='sharedmem')(fill(i) for i in range(self.buff.data.shape[0]))
+            
+        num_of_solved = np.count_nonzero(self.collected)
+        num_of_source = self.collected.shape[0]
+        print(f"Solved symbols: {num_of_solved}/{num_of_source}")
         if not np.all(self.collected):
             print("Blocks are not all recovered, we cannot proceed the file writing.")
             # print unsolved indices of source symbols
-            print(np.count_nonzero(self.collected))
-            print(np.arange(self.collected.shape[0])[~self.collected])
-            return np.count_nonzero(self.collected)
+            print(np.arange(num_of_source)[~self.collected])
+            return num_of_solved
         else:
-            return np.count_nonzero(self.collected)
+            return num_of_solved
             # return self.data[1:]
