b7705d13ea30a9fe5f642e895c8a8d41c8aa051d
jimxuosu <jx@cc.gatech.edu>
2026-05-19 16:24:07 -0400

    fix the bugs
    in main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from encoder import *
 from decoder import *
+import numpy as np
 
 if __name__ == '__main__':
     encoder = LubyEncoder(np.array([0.5, 0.25, 0.25]), 1024, 10000)
     encoder.put_bat(np.ones((1000, 1024), dtype=np.uint8))
-    decoder = LubyDecoder(3, 1024)
+    decoder = IterativeDecoder(3, 1024)
     for i in range(5):
         decoder.put_bat(encoder.get_bat(10000))
     print((decoder.get_all() == np.ones((1000, 1024), dtype=np.uint8)).all())
